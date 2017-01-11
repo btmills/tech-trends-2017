b58e164fa78d6543b01936633d85b07775737706
Brandon Mills <mills.brandont@gmail.com>
2017-01-11 15:47:21 -0500

    Tabs are much better

--- server.js
+++ server.js
@@ -11,21 +11,21 @@ var compiler = webpack(config);
 var serverPort = process.env.PORT || 3000;
 
 app.use(require("webpack-dev-middleware")(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
+	noInfo: true,
+	publicPath: config.output.publicPath
 }));
 
 app.use(require("webpack-hot-middleware")(compiler));
 
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+	res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(serverPort, "localhost", function (err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
+	if (err) {
+		console.log(err);
+		return;
+	}
 
-  console.log("Listening at http://localhost:" + serverPort);
+	console.log("Listening at http://localhost:" + serverPort);
 });
