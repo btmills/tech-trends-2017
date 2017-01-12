7d3fe4c589aa93f6fa14c9af686431ec44975197
Brandon Mills <mills.brandont@gmail.com>
2017-01-11 23:29:48 -0500

    Use slide and fade transitions

--- presentation/index.js
+++ presentation/index.js
@@ -114,7 +114,7 @@ export default class Presentation extends React.Component {
 							margin="20px auto"
 						/>
 					</Slide>
-					<Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
+					<Slide bgImage={images.city.replace("/", "")} bgDarken={0.75}>
 						<Appear fid="1">
 							<Heading size={1} caps fit textColor="primary">
 								Full Width
@@ -131,7 +131,7 @@ export default class Presentation extends React.Component {
 							</Heading>
 						</Appear>
 					</Slide>
-					<Slide transition={["zoom", "fade"]} bgColor="primary">
+					<Slide bgColor="primary">
 						<Heading caps fit>Flexible Layouts</Heading>
 						<Layout>
 							<Fill>
@@ -146,13 +146,13 @@ export default class Presentation extends React.Component {
 							</Fill>
 						</Layout>
 					</Slide>
-					<Slide transition={["slide"]} bgColor="black">
+					<Slide bgColor="black">
 						<BlockQuote>
 							<Quote>Wonderfully formatted quotes</Quote>
 							<Cite>Ken Wheeler</Cite>
 						</BlockQuote>
 					</Slide>
-					<Slide transition={["spin", "zoom"]} bgColor="tertiary">
+					<Slide bgColor="tertiary">
 						<Heading caps fit size={1} textColor="primary">
 							Inline Markdown
 						</Heading>
@@ -167,7 +167,7 @@ You can write inline images, [Markdown Links](http://commonmark.org), paragraph 
 							`}
 						</Markdown>
 					</Slide>
-					<Slide transition={["slide", "spin"]} bgColor="primary">
+					<Slide bgColor="primary">
 						<Heading caps fit size={1} textColor="tertiary">
 							Smooth
 						</Heading>
@@ -175,7 +175,7 @@ You can write inline images, [Markdown Links](http://commonmark.org), paragraph 
 							Combinable Transitions
 						</Heading>
 					</Slide>
-					<Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+					<Slide bgColor="secondary" textColor="primary">
 						<List>
 							<Appear><ListItem>Inline style based theme system</ListItem></Appear>
 							<Appear><ListItem>Autofit text</ListItem></Appear>
@@ -185,13 +185,13 @@ You can write inline images, [Markdown Links](http://commonmark.org), paragraph 
 							<Appear><ListItem>And...</ListItem></Appear>
 						</List>
 					</Slide>
-					<Slide transition={["slide"]} bgColor="primary">
+					<Slide bgColor="primary">
 						<Heading size={1} caps fit textColor="tertiary">
 							Your presentations are interactive
 						</Heading>
 						<Interactive/>
 					</Slide>
-					<Slide transition={["spin", "slide"]} bgColor="tertiary">
+					<Slide bgColor="tertiary">
 						<Heading size={1} caps fit lineHeight={1.5} textColor="primary">
 							Made with love in Seattle by
 						</Heading>
